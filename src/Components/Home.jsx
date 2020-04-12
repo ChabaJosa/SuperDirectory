c19a0112e6d0c6eb03aa3c76b20e655972dab9be
ChabaJosa <chabagjg@gmail.com>
2020-04-12 15:18:16 -0400

    Added random comics to landing page working on filtering the ones with images

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
 
 class Home extends Component {
 
     state = {
         heroeName: "",
+        marvelPublic:"8fc6c25d7b8c17ab22e20964381a9452",
+        marvelPrivate:"d6fa3e44c676e9fcc6339f0d4d84a31bf70e71d0",
+        marvelTs:1,
+        coolHeroesComics:["Daredevil", "Moon Knight", "Thor", "Punisher", "Captain America", "Iron man", "Hulk","Wolverine","Thanos","Spider-Man"],
+        comics:[]
     }
+    
 
     addHeroeProperty = (e) => {
 
@@ -13,14 +22,80 @@ class Home extends Component {
         this.setState({ [key]: value }) // Matches the name="something" with the e.target.name
     }
 
+    componentDidMount= () => {
+        let encryption = require("md5")
+        const randomNumber = (min, max) => {  return Math.floor(Math.random() * (max - min) + min); }  
+        const MarvelApiCall = `https://gateway.marvel.com:443/v1/public/characters?name=${this.state.coolHeroesComics[randomNumber(0,this.state.coolHeroesComics.length-1)]}&ts=${this.state.marvelTs}&apikey=${this.state.marvelPublic}&hash=${encryption(this.state.marvelTs + this.state.marvelPrivate + this.state.marvelPublic)}`
+        axios.get(MarvelApiCall).then(result => {
+            console.log("Full Marvel Result", result.data.data)
+            const ComicApiCall = `https://gateway.marvel.com:443/v1/public/characters/${result.data.data.results[0].id}/comics?&ts=${this.state.marvelTs}&apikey=${this.state.marvelPublic}&hash=${encryption(this.state.marvelTs + this.state.marvelPrivate + this.state.marvelPublic)}`
+            // Filter Data here to find what actually has a thumbnail and a Descriptioni/Title
+            axios.get(ComicApiCall).then( result => {
+                // Should get 3 random comics here 
+                if (result.data.data.results.length > 6){
+                    const picked3 = [result.data.data.results[randomNumber(0,this.state.coolHeroesComics.length-1)],result.data.data.results[randomNumber(0,this.state.coolHeroesComics.length-1)],result.data.data.results[randomNumber(0,this.state.coolHeroesComics.length-1)]]
+                    this.setState({comics:picked3})
+                    console.log("Comics API Request Result", result.data.data)
+                    console.log(this.state)
+                } else {
+                    const picked3 = [result.data.data.results[0],result.data.data.results[1],result.data.data.results[2]]
+                    console.log("Here's the 3 you picked",picked3)
+                    this.setState({comics:picked3})
+                    console.log("Comics API Request Result", result.data.data)
+                    console.log(this.state)
+                }
+                console.log("Comics API Request Result", result.data.data)
+                console.log(this.state)
+            })
+            .catch(() => console.log("Can’t access " + ComicApiCall))
+        })
+        .catch(() => console.log("Can’t access " + MarvelApiCall))
+    }
+
+
+    getAllTheComics = (comics) => { // The parameter should take --> this.props.Comics[0].results
+        let comicsWithImages = comics.filter(everyResult => {
+            return everyResult.thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+        })
+        console.log("These comics have images",comicsWithImages) //All Good Unitl here
+        return (
+            comicsWithImages.map(everyComic => {
+                return (
+                    <div className="card mb-3" style={{ width: "18rem", backgroundColor:"transparent" }}>
+                        <div className="g-card">
+                            <div className="card-container">
+                                <div className="card-front">
+                                    <img className="card-img-top" src={`${everyComic.thumbnail.path}.${everyComic.thumbnail.extension}`} alt="Card image cap" />
+                                    <div className="card-body d-flex align-items-center justify-content-center">
+                                        <p className="card-text">{everyComic.title}</p>
+                                    </div>
+                                </div>
+                                <div className="card-back">
+                                    <div className="backContent">
+                                        {/* <div className="card-text">{ "More Details:"} <Link to={everyComic.urls ? `../${everyComic.urls[0].url}`: "www.marvel.com" } >Click Here</Link></div> */}
+                                        <div className="card-text">{ "More Details:"} <a target="_blank" rel="nofollow" href={everyComic.urls ? `${everyComic.urls[0].url}`: "www.marvel.com" }>Click Here</a></div>
+                                        <br></br>
+                                        {/* <div className="card-text">{ "Want one?:"}    <Link to={everyComic.urls[1] ? `../${everyComic.urls[1].url}`: "www.marvel.com"} >Click Here</Link></div> */}
+                                        <div className="card-text">{ "Want one?:"} <a target="_blank" rel="nofollow" href={everyComic.urls[1] ? `${everyComic.urls[1].url}`: "www.marvel.com" }>Click Here</a></div>                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        )
+    }
+
+
 
     render() {
         // console.log("Home Component", this.state)
-
+        if (this.state.comics.length > 0){
         return (
             <React.Fragment>
 
-                <div className="container">
+                <div className="container" style={{position:"relative", top:"10vh"}}>
 
                     <div className="row py-5">
                         <div className="col-lg-9 mx-auto text-white text-center HomeText">
@@ -38,7 +113,7 @@ class Home extends Component {
 
                                 <form onSubmit={(e) => this.props.handleSubmit(this.state, e)}>
                                     <div className="input-group mb-4">
-                                        <input onChange={this.addHeroeProperty} className="form-control-bs" name="heroeName" type="search" placeholder="Cape Crusaders, Web Crawlers..... we got them all!." aria-describedby="button-addon5" />
+                                        <input onChange={this.addHeroeProperty} className="form-control-bs" name="heroeName" type="search" placeholder="Super Search  !" aria-describedby="button-addon5" />
                                             <div className="input-group-append" onClick={() => this.props.history.push(`Heroes/${this.state.heroeName}`)}>
                                                 <button id="button-addon5" type="submit" className="btn">+</button>
                                             </div>
@@ -50,12 +125,36 @@ class Home extends Component {
                         </div>
                     </div>
 
-
                 </div>
 
+                {/* Comics Here */}
+
+
+                <div className="">
+                    <div className="input-group-append justify-content-center">
+                        <button className="btn btn-danger" type="submit" onClick={() => window.location.reload()}>Random Comcis</button>
+                    </div>
+                    {this.state.coolHeroesComics && 
+                                                
+                            <div className="row d-flex m-4 justify-content-around">
+                                {this.getAllTheComics(this.state.comics)}
+                            </div>
+                    }
+                </div>
 
             </React.Fragment>
+           
         );
+
+        } else{
+            return (
+                <React.Fragment>
+                    <p style={{color:"white"}}>Loading... </p>
+                </React.Fragment>
+        
+            )
+    
+        }
     }
 }
 
